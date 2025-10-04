6ca1432aa281773297dd32a70536473e8be33ed9
POM <peter@omalley.io>
2025-10-04 15:54:32 +0200

    fix: enforce MagicEditModal z-index with inline style
    - Added inline style zIndex: 100003 to DialogPrimitive.Content
    - Ensures modal content appears above MediaLightbox even with CSS specificity issues
    - Previous className z-index wasn't sufficient due to Radix Dialog stacking context

--- src/shared/components/MagicEditModal.tsx
+++ src/shared/components/MagicEditModal.tsx
@@ -261,7 +261,7 @@ const MagicEditModal: React.FC<MagicEditModalProps> = ({
               "fixed left-[50%] top-[50%] z-[100003] grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
               modal.className
             )}
-            style={modal.style}
+            style={{ ...modal.style, zIndex: 100003 }}
             {...{...modal.props}}
             onPointerDown={(e) => {
               // Block propagation from modal content to underlying elements
